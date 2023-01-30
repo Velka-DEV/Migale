9d647acccabd66624d3031f4aa9ddbc763f1a573
0x426c61636b486f6c65 <velka.pro@gmail.com>
2023-01-30 09:25:57 +0100

    HttpClient crawler implementation

---
+++ src/Migale.HttpClient/Extensions/MigaleBuilderExtension.cs
@@ -0,0 +1,23 @@
+﻿using Migale.Core;
+using Migale.Core.Models;
+
+namespace Migale.HttpClient.Extensions;
+
+public static class MigaleBuilderExtensions
+{
+    public static MigaleBuilder WithHttpClientCrawler(this MigaleBuilder builder)
+    {
+        builder.WithCrawler<HttpClientCrawler>();
+        
+        return builder;
+    }
+
+    public static MigaleBuilder WithHttpClientCrawler(this MigaleBuilder builder, IEnumerable<Proxy> proxies)
+    {
+        return builder.WithCrawlers(proxies.Select(p => new HttpClientCrawler(new System.Net.Http.HttpClient(new HttpClientHandler
+        {
+            Proxy = p.ToWebProxy(),
+            UseProxy = true
+        }))));
+    }
+}
---
+++ src/Migale.HttpClient/HttpClientCrawler.cs
@@ -0,0 +1,23 @@
+﻿using Migale.Core.Crawlers;
+using Migale.Core.Models;
+
+namespace Migale.HttpClient;
+
+public class HttpClientCrawler : ICrawler
+{
+    private readonly System.Net.Http.HttpClient _httpClient;
+
+    public HttpClientCrawler() : this(new System.Net.Http.HttpClient()) { }
+    
+    public HttpClientCrawler(System.Net.Http.HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    
+    public async Task<string?> CrawlAsync(HttpTarget target, CancellationToken token = default)
+    {
+        using var request = target.BuildRequest();
+        using var response = await _httpClient.SendAsync(request, token);
+        return await response.Content.ReadAsStringAsync(token);
+    }
+}
